74c7f0a0bd5508cb7cd387ec5700bd90e60be3f2
Aman Kumar Singh <amankumarsingh.professional@gmail.com>
2022-02-25 00:37:26 +0530

    add some logic

--- Qualification/solution.py
+++ Qualification/solution.py
@@ -1,12 +1,32 @@
+def quick_sort(projects_name, projects):
+    if len(projects_name)<=1:
+        return projects_name
+    else:
+        pivot=projects[projects_name[0]]['S']
+        print(pivot)
+        less=[]
+        greater=[]
+        for i in range(1,len(projects_name)):
+            if projects[projects_name[i]]['S']<pivot:
+                less.append(projects_name[i])
+            else:
+                greater.append(projects_name[i])
+        print(less,greater)
+        return quick_sort(less,projects)+[projects_name[0]]+quick_sort(greater,projects)
+
 import os, sys, math
+
+#input
 
 input_data=open(sys.argv[1], 'r').read().splitlines()
 
 CP=list(map(int,input_data[0].split(' ')))
-print(CP)
+#print(CP)
 i=1
 contributors=dict()
+contributors_name=[]
 projects=dict()
+projects_name=[]
 
 #
 #{BOB:[skill1{name:level}, skill2, skill3]}
@@ -15,6 +35,7 @@ projects=dict()
 for j in range(CP[0]):
     contributors[input_data[i].split(' ')[0]]=[]
     name=input_data[i].split(' ')[0]
+    contributors_name[name]=[]
     for k in range(int(input_data[i].split(' ')[1])):
         contributors[name].append(input_data[i+k+1])
     i+=int(input_data[i].split(' ')[1])+1
@@ -25,6 +46,7 @@ for j in range(CP[1]):
     #print(input_data[i])
     projects[input_data[i].split(' ')[0]]=dict()
     name=input_data[i].split(' ')[0]
+    projects_name.append(name)
     projects[name]['D']=int(input_data[i].split(' ')[1])
     projects[name]['S']=int(input_data[i].split(' ')[2])
     projects[name]['B']=int(input_data[i].split(' ')[3])
@@ -34,3 +56,14 @@ for j in range(CP[1]):
         projects[name]['roles'].append(input_data[i+k+1])
     i+=projects[name]['R']+1
 print(projects)
+
+#logic
+#quick sort
+projects_name=quick_sort(projects_name, projects)
+projects_name.reverse()
+print(projects_name)
+print(projects[projects_name[0]])
+
+for i in range(len(projects_name)):
+    for role in projects[projects_name[i]]['roles']:
+        
